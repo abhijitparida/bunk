22f15a6042e27551c30ad829bc547622d6452a94
Abhijit Parida <abhijitparida.me@gmail.com>
2017-03-06 18:10:33 +0530

    Get session cookies and login

--- apidebug.sh
+++ apidebug.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+# get session cookies and XSRF token
+curl -s -c /tmp/cookies.txt > /dev/null \
+    http://111.93.164.203/CampusPortalSOA/index
+XFRF=$(grep XSRF-TOKEN /tmp/cookies.txt | awk '{print $7}')
+
 read -p "username: " username
 read -sp "password: " password
+echo -e "\nlogging in $username\n"
 
-echo -e "\nlogging in $username with password $password"
+# login
+curl -s -b /tmp/cookies.txt -X POST \
+    -H "X-XSRF-TOKEN: $XFRF" \
+    -H "Content-Type:application/json;charset=UTF-8" \
+    -d "{username: \"$username\", password: \"$password\"}" \
+    http://111.93.164.203/CampusPortalSOA/login
+
+# uncomment to download student photo
+# curl -s -b /tmp/cookies.txt > /tmp/profile.jpg \
+#     http://111.93.164.203/CampusPortalSOA/image/studentPhoto
+
+# cleanup
+rm -f /tmp/cookies.txt
