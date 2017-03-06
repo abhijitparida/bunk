21dc6bf0cfe86833ba22c74b7da0bbb2a97e6225
Abhijit Parida <abhijitparida.me@gmail.com>
2017-03-06 18:49:27 +0530

    Get student info and attendance

--- apidebug.sh
+++ apidebug.sh
@@ -15,10 +15,25 @@ curl -s -b /tmp/cookies.txt -X POST \
     -H "Content-Type:application/json;charset=UTF-8" \
     -d "{username: \"$username\", password: \"$password\"}" \
     http://111.93.164.203/CampusPortalSOA/login
+echo -e "\n"
 
 # uncomment to download student photo
 # curl -s -b /tmp/cookies.txt > /tmp/profile.jpg \
 #     http://111.93.164.203/CampusPortalSOA/image/studentPhoto
+
+# get student info
+curl -s -b /tmp/cookies.txt -X POST \
+    -H "X-XSRF-TOKEN: $XFRF" \
+    http://111.93.164.203/CampusPortalSOA/attendanceinfo
+echo -e "\n"
+
+# get attendance
+curl -s -b /tmp/cookies.txt -X POST \
+    -H "X-XSRF-TOKEN: $XFRF" \
+    -H "Content-Type:application/json;charset=UTF-8" \
+    -d "{registerationid: \"ITERRETD1612A0000002\"}" \
+    http://111.93.164.203/CampusPortalSOA/attendanceinfo
+echo -e "\n"
 
 # cleanup
 rm -f /tmp/cookies.txt
