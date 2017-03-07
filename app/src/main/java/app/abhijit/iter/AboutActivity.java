7267d636cbe68f048c19aee3c9bac54dfa2e25aa
Abhijit Parida <abhijitparida.me@gmail.com>
2017-03-07 18:04:30 +0530

    Don't toast "debug build" in About activity

--- app/src/main/java/app/abhijit/iter/AboutActivity.java
+++ app/src/main/java/app/abhijit/iter/AboutActivity.java
@@ -24,32 +24,22 @@
 
 package app.abhijit.iter;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
-
-    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        mContext = this;
-
         setupToolbar();
         setupIcon();
-
-        if (BuildConfig.DEBUG) {
-            Toast.makeText(mContext, "debug build", Toast.LENGTH_SHORT).show();
-        }
     }
 
     @Override
