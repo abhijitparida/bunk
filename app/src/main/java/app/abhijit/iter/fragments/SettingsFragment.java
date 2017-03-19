295b1a2536777b9412d80f2acc62f05964036bba
Abhijit Parida <abhijitparida.me@gmail.com>
2017-03-19 11:41:05 +0530

    Separate SettingsFragment

--- app/src/main/java/app/abhijit/iter/fragments/SettingsFragment.java
+++ app/src/main/java/app/abhijit/iter/fragments/SettingsFragment.java
@@ -22,46 +22,18 @@
  * THE SOFTWARE.
  */
 
-package app.abhijit.iter;
+package app.abhijit.iter.fragments;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.preference.PreferenceFragment;
 
-import app.abhijit.iter.fragments.SettingsFragment;
+import app.abhijit.iter.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsFragment extends PreferenceFragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        setupToolbar();
-        setupPreferences();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            finish();
-        }
-
-        return true;
-    }
-
-    private void setupToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-    }
-
-    private void setupPreferences() {
-        getFragmentManager().beginTransaction().replace(R.id.settings_fragment, new SettingsFragment()).commit();
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
