4512b8a129c216c1377226d3322090304dcc347c
Abhijit Parida <abhijitparida.me@gmail.com>
2017-08-17 20:06:08 +0530

    Finalize attendance activity design

--- app/src/main/java/app/abhijit/iter/AttendanceActivity.java
+++ app/src/main/java/app/abhijit/iter/AttendanceActivity.java
@@ -26,7 +26,9 @@ package app.abhijit.iter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -36,19 +38,20 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+
+import java.util.Random;
 
 public class AttendanceActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
     private Context mContext;
-    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,108 +65,88 @@ public class AttendanceActivity extends AppCompatActivity
 
         if (!BuildConfig.DEBUG) {
             displayBannerAd();
-            setupInterstitialAd();
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        /*if (!((MainApplication) getApplication()).isLoggedIn()) {
-            startActivity(new Intent(AttendanceActivity.this, LoginActivity.class));
-            return;
-        }*/
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.three_dots, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        if (id == R.id.action_about) {
-            startActivity(new Intent(AttendanceActivity.this, AboutActivity.class));
-        } else if (id == R.id.action_feedback) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.url_feedback))));
+        if (id == R.id.action_refresh) {
+            Toast.makeText(mContext, ":^)", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.action_logout) {
+            Toast.makeText(mContext, "Logged out", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(AttendanceActivity.this, LoginActivity.class));
+            finish();
+        } else if (id == R.id.action_settings) {
+            startActivity(new Intent(AttendanceActivity.this, SettingsActivity.class));
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-
-        if (id == R.id.nav_settings) {
-            startActivity(new Intent(AttendanceActivity.this, SettingsActivity.class));
-        } else if (id == R.id.nav_logout) {
-            performLogout();
-            if (!BuildConfig.DEBUG && mInterstitialAd.isLoaded()) {
-                mInterstitialAd.show();
-            } else {
-                startActivity(new Intent(AttendanceActivity.this, LoginActivity.class));
-            }
+        if (id == R.id.nav_about) {
+            startActivity(new Intent(AttendanceActivity.this, AboutActivity.class));
+        } else if (id == R.id.nav_feedback) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.url_feedback))));
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            finish();
         }
     }
 
     private void setupToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
     private void setupDrawer() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        int prompts[] = {
+                R.string.nav_opensource,
+                R.string.nav_hacking,
+                R.string.nav_coding,
+                R.string.nav_programming
+        };
+        TextView opensource = drawer.findViewById(R.id.opensource);
+        opensource.setText(prompts[new Random().nextInt(prompts.length)]);
+        TextView github = drawer.findViewById(R.id.github);
+        github.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.url_github))));
+            }
+        });
     }
 
     private void displayBannerAd() {
         MobileAds.initialize(mContext, getResources().getString(R.string.banner_ad_unit_id));
-        AdView adView = (AdView) findViewById(R.id.ad);
+        AdView adView = findViewById(R.id.ad);
         adView.loadAd(new AdRequest.Builder().build());
-    }
-
-    private void setupInterstitialAd() {
-        mInterstitialAd = new InterstitialAd(mContext);
-        //mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
-
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                mInterstitialAd.loadAd(new AdRequest.Builder().build());
-                startActivity(new Intent(AttendanceActivity.this, LoginActivity.class));
-            }
-        });
-
-        mInterstitialAd.loadAd(new AdRequest.Builder().build());
-    }
-
-    private void performLogout() {
-        /*((MainApplication) getApplication()).setLoggedIn(false);*/
-        Toast.makeText(mContext, "Logged out", Toast.LENGTH_SHORT).show();
     }
 }
