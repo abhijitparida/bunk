0a0df3886eac3b93666a8becdc2acf74a81fd74a
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-10-14 19:13:36 -0300

    Add tests to CookieJar class

--- app/src/test/java/app/abhijit/iter/data/CookieJarTest.java
+++ app/src/test/java/app/abhijit/iter/data/CookieJarTest.java
@@ -1,6 +1,40 @@
 package app.abhijit.iter.data;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collections;
+import java.util.List;
+import okhttp3.Cookie;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CookieJarTest {
 
-    // TODO: Add tests using Mockito
+    private CookieJar cookieJar;
+
+    @Before
+    public void setUp() {
+        cookieJar = new CookieJar();
+    }
+
+    @Test
+    public void saveFromResponse_ValidCookieList_PopulatesCookieStoreAccordingly() {
+        cookieJar.saveFromResponse(null, createCookies());
+        List<Cookie> cookieStore = cookieJar.loadForRequest(null);
+
+        assertEquals(1, cookieStore.size());
+    }
+
+    @Test
+    public void loadForRequest_InitialState_ReturnEmptyCookieStore() {
+        List<Cookie> cookieStore = cookieJar.loadForRequest(null);
+
+        assertTrue(cookieStore.isEmpty());
+    }
+
+    private List<Cookie> createCookies() {
+        return Collections.singletonList(mock(Cookie.class));
+    }
 }
